93e3dce3b49e4f1290c4a6ff4fec1660ca549aae
Suraj Lad <surajlad9@gmail.com>
2020-12-06 22:54:02 +0530

    APK Added | Splash Updated

--- lib/ui/splash_page.dart
+++ lib/ui/splash_page.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:xno_taskapp/helpers/app_constants.dart';
 import 'package:xno_taskapp/helpers/layout_helper.dart';
+import 'package:xno_taskapp/helpers/text_styles.dart';
 import 'package:xno_taskapp/ui/home_page.dart';
 
 class SplashPage extends StatefulWidget {
@@ -16,12 +17,10 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     Timer(Duration(seconds: 3), () async {
-      LayoutHelper.instance.width = Get.width;
-      LayoutHelper.instance.height = Get.height;
-      LayoutHelper.instance.fontSize = Get.height > 700 ? 16 : 15;
       LayoutHelper.instance.taskList = await Hive.openBox(AppConstants.taskBox);
       LayoutHelper.instance.userList = await Hive.openBox(AppConstants.userBox);
-      LayoutHelper.instance.labelList = await Hive.openBox(AppConstants.labelBox);
+      LayoutHelper.instance.labelList =
+          await Hive.openBox(AppConstants.labelBox);
 
       Get.to(HomePage());
     });
@@ -30,10 +29,26 @@ class _SplashPageState extends State<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
+    LayoutHelper.instance.width = Get.width;
+    LayoutHelper.instance.height = Get.height;
+    LayoutHelper.instance.fontSize = Get.height > 700 ? 16 : 15;
     return Scaffold(
       backgroundColor: AppConstants.appBackgroundColor,
       body: Center(
-        child: Text("Task App"),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/task_splash.png',
+              width: Get.width * 0.75,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Task Manager",
+              style: largeBoldTxt,
+            ),
+          ],
+        ),
       ),
     );
   }
